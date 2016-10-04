db3a022aaa9814962d9e0685850a1da347d1463e
Marcin <me@domain.com>
2016-10-04 18:18:09 +0200

    * Scripts moved from subfolders to Scripts folder * Scripts from _CompletedAssets have CompleteProject namespace

--- Assets/_CompletedAssets/Scripts/Audio/MixLevels.cs
+++ Assets/_CompletedAssets/Scripts/Audio/MixLevels.cs
@@ -1,18 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.Audio;
-
-public class MixLevels : MonoBehaviour {
-
-	public AudioMixer masterMixer;
-
-	public void SetSfxLvl(float sfxLvl)
+namespace CompleteProject
+{
+	public class MixLevels : MonoBehaviour
 	{
-		masterMixer.SetFloat("sfxVol", sfxLvl);
-	}
 
-	public void SetMusicLvl (float musicLvl)
-	{
-		masterMixer.SetFloat ("musicVol", musicLvl);
+		public AudioMixer masterMixer;
+
+		public void SetSfxLvl(float sfxLvl)
+		{
+			masterMixer.SetFloat("sfxVol", sfxLvl);
+		}
+
+		public void SetMusicLvl(float musicLvl)
+		{
+			masterMixer.SetFloat("musicVol", musicLvl);
+		}
 	}
 }
--- Assets/_CompletedAssets/Scripts/Managers/PauseManager.cs
+++ Assets/_CompletedAssets/Scripts/Managers/PauseManager.cs
@@ -5,55 +5,58 @@ using UnityEngine.Audio;
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
+namespace CompleteProject
+{
+	public class PauseManager : MonoBehaviour
+	{
 
-public class PauseManager : MonoBehaviour {
-	
-	public AudioMixerSnapshot paused;
-	public AudioMixerSnapshot unpaused;
-	
-	Canvas canvas;
-	
-	void Start()
-	{
-		canvas = GetComponent<Canvas>();
-	}
-	
-	void Update()
-	{
-		if (Input.GetKeyDown(KeyCode.Escape))
+		public AudioMixerSnapshot paused;
+		public AudioMixerSnapshot unpaused;
+
+		Canvas canvas;
+
+		void Start()
 		{
-			canvas.enabled = !canvas.enabled;
-			Pause();
+			canvas = GetComponent<Canvas>();
 		}
-	}
-	
-	public void Pause()
-	{
-		Time.timeScale = Time.timeScale == 0 ? 1 : 0;
-		Lowpass ();
-		
-	}
-	
-	void Lowpass()
-	{
-		if (Time.timeScale == 0)
+
+		void Update()
 		{
-			paused.TransitionTo(.01f);
+			if (Input.GetKeyDown(KeyCode.Escape))
+			{
+				canvas.enabled = !canvas.enabled;
+				Pause();
+			}
 		}
-		
-		else
-			
+
+		public void Pause()
 		{
-			unpaused.TransitionTo(.01f);
+			Time.timeScale = Time.timeScale == 0 ? 1 : 0;
+			Lowpass();
+
 		}
-	}
-	
-	public void Quit()
-	{
-		#if UNITY_EDITOR 
-		EditorApplication.isPlaying = false;
-		#else 
+
+		void Lowpass()
+		{
+			if (Time.timeScale == 0)
+			{
+				paused.TransitionTo(.01f);
+			}
+
+			else
+
+			{
+				unpaused.TransitionTo(.01f);
+			}
+		}
+
+		public void Quit()
+		{
+#if UNITY_EDITOR
+			EditorApplication.isPlaying = false;
+#else
 		Application.Quit();
-		#endif
+#endif
+		}
 	}
 }
