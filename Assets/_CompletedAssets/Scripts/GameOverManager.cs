9182a6e0a7b1faf1d90dbe171eae5561ffe04e00
Marcin <me@domain.com>
2016-10-06 20:51:08 +0200

    * CompleteProject scipts formatted

--- Assets/_CompletedAssets/Scripts/GameOverManager.cs
+++ Assets/_CompletedAssets/Scripts/GameOverManager.cs
@@ -2,29 +2,29 @@
 
 namespace CompleteProject
 {
-    public class GameOverManager : MonoBehaviour
-    {
-        public PlayerHealth playerHealth;       // Reference to the player's health.
+	public class GameOverManager : MonoBehaviour
+	{
+		public PlayerHealth playerHealth;       // Reference to the player's health.
 
 
-        Animator anim;                          // Reference to the animator component.
+		Animator anim;                          // Reference to the animator component.
 
 
-        void Awake ()
-        {
-            // Set up the reference.
-            anim = GetComponent <Animator> ();
-        }
+		void Awake()
+		{
+			// Set up the reference.
+			anim = GetComponent<Animator>();
+		}
 
 
-        void Update ()
-        {
-            // If the player has run out of health...
-            if(playerHealth.currentHealth <= 0)
-            {
-                // ... tell the animator the game is over.
-                anim.SetTrigger ("GameOver");
-            }
-        }
-    }
+		void Update()
+		{
+			// If the player has run out of health...
+			if (playerHealth.currentHealth <= 0)
+			{
+				// ... tell the animator the game is over.
+				anim.SetTrigger("GameOver");
+			}
+		}
+	}
 }
