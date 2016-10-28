ee523f9325af75453a9d7d9aca8ea7d8cbb33a91
Marcin <me@domain.com>
2016-10-28 06:03:52 +0200

    + MenuCanvas with sound volume sliders * Prefabs with mixer audio source output

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,8 +5,8 @@ namespace FenceShooter {
 	public class CameraFollow : MonoBehaviour {
 		public Transform target;            // The position that that camera will be following.
 		public float smoothing = 5f;        // The speed with which the camera will be following.
-		[Tooltip("AudioListener.volume"), Range(0f, 1f)]
-		public float soundVolume = 1f;
+		//[Tooltip("AudioListener.volume"), Range(0f, 1f)]
+		//public float soundVolume = 1f;
 
 		Vector3 offset;                     // The initial offset from the target.
 
@@ -20,9 +20,9 @@ namespace FenceShooter {
 		void FixedUpdate() {
 			// Set AudioListener's volume
 #if UNITY_EDITOR
-			if (AudioListener.volume != soundVolume) {
-				AudioListener.volume = soundVolume;
-			}
+			//if (AudioListener.volume != soundVolume) {
+			//	AudioListener.volume = soundVolume;
+			//}
 #endif
 			// Create a postion the camera is aiming for based on the offset from the target.
 			Vector3 targetCamPos = target.position + offset;
--- Assets/_CompletedAssets/Scripts/MixLevels.cs
+++ Assets/_CompletedAssets/Scripts/MixLevels.cs
@@ -1,10 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.Audio;
+using UnityEngine.UI;
+
 namespace SurvivalShooter {
 	public class MixLevels : MonoBehaviour {
-
 		public AudioMixer masterMixer;
+		public Slider sfxVolSlider;
+		public Slider musicVolSlider;
+
+		void Start() {
+			if (sfxVolSlider) {
+				sfxVolSlider.value = Mathf.Lerp(sfxVolSlider.minValue, sfxVolSlider.maxValue, 0.5f);
+			}
+			if (musicVolSlider) {
+				musicVolSlider.value = Mathf.Lerp(musicVolSlider.minValue, musicVolSlider.maxValue, 0.5f);
+			}
+		}
 
 		public void SetSfxLvl(float sfxLvl) {
 			masterMixer.SetFloat("sfxVol", sfxLvl);
